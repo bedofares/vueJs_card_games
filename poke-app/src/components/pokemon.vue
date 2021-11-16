07ace3d6ee7b6e42aaf9451b0541b9b59e8fbe3b
cc201013 <cc201013.fhstp.ac.at>
2021-11-16 02:22:42 +0100

    Port the Pokemon to individual components

---
+++ poke-app/src/components/pokemon.vue
@@ -0,0 +1,39 @@
+<script>
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+
+// Define a new global component called button-counter
+export default {
+  name :'pokemon-tag',
+  data(){
+  return{
+    pokemon:[],
+  }
+},
+  methods:{
+  async catchAll(){
+    const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+    const data = await fetch(url);
+    const pokemon = await data.json();
+    this.pokemon = pokemon.results;
+  }
+},
+}
+</script>
+
+<template>
+  <p class="text-center"> <button class="btn btn-danger mb-5 " @click="catchAll">Catch Pokemon</button></p>
+
+  <div v-for="(item, index) in pokemon" :key="index">
+    <div>
+      <img
+          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            index + 1
+          }.png`"
+          alt=""
+      />
+      <h2>{{ item.name }}</h2>
+    </div>
+  </div>
+</template>
--- poke-app/src/pages/Pokedex.vue
+++ poke-app/src/pages/Pokedex.vue
@@ -1,39 +1,19 @@
 <script>
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import Pokemon from '../components/pokemon.vue'
 
 // Define a new global component called button-counter
 export default {
   name :'PokedexPage',
-  data(){
-    return{
-      pokemon:[],
-    }
-  },
-  methods:{
-    async catchAll(){
-      const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-      const data = await fetch(url);
-      const pokemon = await data.json();
-      this.pokemon = pokemon.results;
-    }
-  },
+  components:{
+   Pokemon,
+  }
 }
 </script>
 
 <template>
   <h1 class="text-muted text-center">Pokedex Page</h1>
-  <button class="mb-5" @click="catchAll">Catch Pokemon</button>
-  <div v-for="(item, index) in pokemon" :key="index">
-    <div>
-      <img
-          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            index + 1
-          }.png`"
-          alt=""
-      />
-      <h2>{{ item.name }}</h2>
-    </div>
-  </div>
+  <Pokemon></Pokemon>
 <!-- {{pokemon}}-->
 </template>
