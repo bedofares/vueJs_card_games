c20d0ba9638cfdb3786b1f41b29e3316fe09ba6b
cc201013 <cc201013.fhstp.ac.at>
2021-11-16 01:46:19 +0100

    vite app

---
+++ poke-app/src/main.js
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+//import App from './App.vue'
+import { createRouter , createWebHistory } from 'vue-router'
+import Home from './pages/Home.vue'
+import Pokedex from './pages/Pokedex.vue'
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/pokedex', component: Pokedex }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+// 5. Create and mount the root instance.
+const app = createApp({
+
+})
+
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+app.mount('#app')
